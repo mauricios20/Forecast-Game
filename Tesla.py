759de3f70a409d608c1afe892f46839a331f377b
Mauricio Solorio <56321447+mauricios20@users.noreply.github.com>
2022-03-05 09:25:14 -0800

    Final week

--- Tesla.py
+++ Tesla.py
@@ -59,10 +59,21 @@ low5 = round(hist['Close'].min(), 2)
 
 print('Week 5 - Average: {0:}, High: {1:}, Low: {2:}'.format(ave5, high5, low5))
 
+####### Week 6 #######
+
+hist = tsla.history(start="2022-02-28", end="2022-03-05")
+print(hist['Close'])
+
+ave6 = round(hist['Close'].mean(), 2)
+high6 = round(hist['Close'].max(), 2)
+low6 = round(hist['Close'].min(), 2)
+
+print('Week 6 - Average: {0:}, High: {1:}, Low: {2:}'.format(ave6, high6, low6))
+
 #  #################### Read and Clean Data ################################
-lst = [['High', high, high2, high3, high4, high5], ['Average', ave, ave2, ave3, ave4, ave5],
-       ['Low', low, low2, low3, low4, low5]]
-tesladtf = pd.DataFrame(lst, columns=['Value', 'Week 1', 'Week 2', 'Week 3', 'Week 4', 'Week 5'])
+lst = [['High', high, high2, high3, high4, high5, high6], ['Average', ave, ave2, ave3, ave4, ave5, ave6],
+       ['Low', low, low2, low3, low4, low5, low6]]
+tesladtf = pd.DataFrame(lst, columns=['Value', 'Week 1', 'Week 2', 'Week 3', 'Week 4', 'Week 5', 'Week 6'])
 print(tesladtf)
 
 path = '/Users/mau/Dropbox/Mac/Documents/Econ 103/Spring 2022/Forecast Game'
